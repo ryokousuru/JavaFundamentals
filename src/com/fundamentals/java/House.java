88db61aedf7387b830bbdf5cadeac570c915e936
Marvin Ziegler <sqreroot49@gmail.com>
2019-06-13 16:02:02 -0500

    Lesson 04 My Application Overview

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,17 @@
+package com.fundamentals.java;
+/*
+ This is our first class that we create ourselves
+ */
+
+public class House {
+
+    String roofType;
+    int windowSize;
+    String foundation;
+    String doorColor;
+
+    //Our first method (function)
+    public void doorOpenClose(){
+        System.out.println("Open my front door.");
+    }
+}
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -1,8 +1,15 @@
 package com.fundamentals.java;
+/*
+*This class is the starting class of this project
+* This is an example of a multi-line comment
+*/
 
 public class Main {
 
     public static void main(String[] args) {
+        //write your code here
         System.out.println("Welcome to Java");
+        House myHouse = new House();
+        myHouse.doorOpenClose();
     }
 }
